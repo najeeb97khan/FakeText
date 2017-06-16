70b2181daac5158e41c9d7e0c9d1e02f65a8c013
najeeb97khan <najeeb.khan96@gmail.com>
2017-06-16 17:29:00 +0530

    Text generator function added

--- src/generator.py
+++ src/generator.py
@@ -15,10 +15,11 @@ from make_batches import *
 from encode_decode import *
 
 ## Defining Constants
-HIDDEN_SIZE = 5
+HIDDEN_SIZE = 200
 NUM_STEPS = 60
 BATCH_SIZE = 64
 LEARNING_RATE = 1e-3
+LEN_SENTENCE = 50
 SKIP_STEP = 100
 DATA_PATH = '../data/arvix_abstracts.txt'
 
@@ -46,14 +47,21 @@ def constructModel(vocab):
     with tf.variable_scope("loss") as scope:
         loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=logits[:,:-1], labels=seq_one_hot[:,1:]))
     
-    return seq, loss, output, out_state, in_state
+    return seq, loss, output, out_state, in_state, logits
 
 def createOptimizer(loss, global_step, name="optimizer"):
     with tf.variable_scope(name) as scope:
         optimizer = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss, global_step=global_step)
         return optimizer
 
-def train(seq, vocab, loss, optimizer, global_step):
+def createSummaries(loss, loss_name):
+    with tf.variable_scope("summaries") as scope:
+        tf.summary.scalar(loss_name, loss)
+        tf.summary.histogram(loss_name, loss)
+        summary_op = tf.summary.merge_all()
+        return summary_op
+
+def train(seq, vocab, loss, optimizer, summary_op, in_state, out_state, logits, global_step):
 
     saver = tf.train.Saver()
     init = tf.global_variables_initializer()
@@ -74,18 +82,31 @@ def train(seq, vocab, loss, optimizer, global_step):
         start = time.time()
         iteration = global_step.eval()
         for batch in read_batch(read_data(DATA_PATH, vocab)):
-            batch_loss = sess.run([loss, optimizer], feed_dict={seq:batch})
+            batch_loss, summary, _ = sess.run([loss, summary_op, optimizer], feed_dict={seq:batch})
 
-            if iteration % SKIP_STEP:
+            if iteration % SKIP_STEP == 0:
                 print 'Iteration:{}\n Loss: {}\tTime: {}'.format(iteration, batch_loss, time.time() - start)
-                generate_text(sess, vocab, seed='T')
+                generate_text(sess, vocab, in_state, out_state, seq, logits, seed='T')
                 start = time.time()
                 saver.save(sess, '../checkpoints/arXiv/fake', iteration)
             iteration += 1
+            writer.add_summary(summary, iteration)
 
-def generate_text(sess, vocab, seed='T'):
+def generate_text(sess, vocab, in_state, out_state, seq, logits, seed='T'):
     
-    print 'Text generation function not implemented...'
+    sentence = seed
+    state = None
+    for _ in range(LEN_SENTENCE):
+        
+        batch = [encode_vocab(sentence[-1], vocab)]
+        feed = {seq:batch}
+        if state is not None:
+            feed.update({in_state:state})
+        output, state = sess.run([logits, out_state], feed)
+        inx = np.argmax(np.reshape(np.asarray(output),[1, len(vocab)]))
+        sentence += decode_vocab([inx], vocab)
+    
+    print sentence
     return
 
 def main():
@@ -94,10 +115,11 @@ def main():
 
     global_step = tf.Variable(0, tf.float32, name="global_step")
     print 'Constructing the model'
-    seq, loss, output, out_state, in_state = constructModel(vocab)
+    seq, loss, output, out_state, in_state, logits = constructModel(vocab)
     optimizer = createOptimizer(loss, global_step)
+    summary_op = createSummaries(loss, "xentropy_loss")
     print 'Training the model'
-    train(seq, vocab, loss, optimizer, global_step)
+    train(seq, vocab, loss, optimizer, summary_op, in_state, out_state, logits, global_step)
 
 
 if __name__ == "__main__":
